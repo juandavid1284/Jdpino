636d0c96f4a598ef64eebbc80f9f5c572396ae79
Pino <jdavidpino@hotmail.com>
2021-08-22 22:35:37 -0500

    Responses

--- jdpino.Common/Responses/Response.cs
+++ jdpino.Common/Responses/Response.cs
@@ -6,11 +6,9 @@ namespace jdpino.Common.Responses
 {
     public class Response
     {
-        public bool IsSuccess { get; set; }
+        public bool IsSucces { get; set; }
         public string Message { get; set; }
-
         public object Result { get; set; }
 
     }
-
 }
